17a6a52f976c23b405b254053bc63ea12d0b4366
= <918780@dpsk12.net>
2022-08-19 16:07:19 -0600

    set to default vue config

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,41 @@
-import {resolve,dirname} from 'path'
+
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url';
-
-const _dirname = dirname(fileURLToPath(import.meta.url));
-
-console.log(_dirname)
-
-const root = resolve(_dirname,'src')
-const outDir = resolve(_dirname,'dist')
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root,
-    build:{
-        outDir,
-        emptyOutDir:true,
-        rollupOptions:{
-            input:{
-                main:resolve(root,'main','index.html'),
-                contact:resolve(root,'contact','index.html'),
-                first:resolve(root,'first','index.html'),
-                sponsor:resolve(root,'sponsor','index.html'),
-            }
-        }
+	pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    subpage: 'src/subpage/main.js'
+  },
+  server: {
+    host: true
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
 })
+
+
