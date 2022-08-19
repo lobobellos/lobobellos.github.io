bf61ca23b8f4965b2fa4fa71bb5fb6b100b552e5
= <918780@dpsk12.net>
2022-08-19 16:10:53 -0600

    replaced html with sfc

---
+++ src/pages/Sponsors.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="body">
+        <div class="textContainer">
+            <p id="aboutUs">
+                our sponsors are
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+	.body{
+    background-color:rgb(62, 16, 88);
+    text-align: center;
+}
+
+.body .textContainer{
+    width:80%;
+    margin: auto;
+    max-width: 600px;
+}
+
+.body .textContainer p{
+    height:200px;
+    color:white;
+    font-family: 'Figtree', sans-serif;
+}
+</style>
