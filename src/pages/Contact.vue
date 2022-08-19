2a72c91693df002ea83cd8644120caa37b7461a5
= <918780@dpsk12.net>
2022-08-19 16:09:20 -0600

    replaced html with sfc

---
+++ src/pages/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <table>
+<thead>
+  <tr>
+    <th>Maryam Rahimie</th>
+    <th>Team Affairs</th>
+    <th>m.rhimie2@gmail.com (720)-276-3416 (cell)</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>Robert Davis Jr</td>
+    <td>(Team Affairs)</td>
+    <td>rob2davis2@gmail.com (720)-815-8794 (cell)</td>
+  </tr>
+  <tr>
+    <td>Matthew Lapaire</td>
+    <td>(Lead Mentor)</td>
+    <td>matthew_lapaire@dpsk12.net</td>
+  </tr>
+</tbody>
+</table>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+		.body{
+    background-color:rgb(62, 16, 88);
+    text-align: center;
+}
+
+
+</style>
