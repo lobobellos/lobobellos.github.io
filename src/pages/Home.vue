d26da7a29abdfb8007de06060b368cfbffa8fbe5
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-09-13 23:08:48 +0000

    added what we do paragraph

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,6 +5,15 @@
 			<img src='../assets/teamPic.jpg'>
 		
 				<p>FRC team 7243, the "Robo-Lobo-Bellos", strives to provide students with STEM education, problem solving, management, and leadership skills. We accomplish this by providing a student-led and student-focused team structure where people can find their place. Our family-like culture drives us to take pride in our team and give it our all.</p>
+
+		<h2>What We Do</h2>
+
+		
+
+		<iframe src="https://www.youtube.com/embed/akLmtVX5skA" title="Qualification 59 - 2022 Utah Regional" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+			<p>Our team competes in FRC, the FIRST Robotics Competition. Each year, our team designs, builds, programs, and tests a new robot, which is able to complete tasks and score points on the game field.</p>
+		
     </div>
 </template>
 
@@ -32,6 +41,11 @@ p{
     margin: auto;
     max-width: 600px;
 }
+	iframe{
+		width:500px;
+		height: 300px;
+		margin-bottom:30px;
+	}
 
 
 
