7c81a8527933a98434733d720683916e29f551fb
bakedpotatolord <918780@dpsk12.net>
2022-12-17 20:15:04 -0700

    add image alt tags

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,9 @@
 	<div class="body">
 			<h2>About Us</h2>
 
-			<img src='../assets/teamPic.jpg'>
+			<img src='../assets/teamPic.jpg' alt="Team Photo">
 		
-				<p>FRC team 7243, the "Robo-Lobo-Bellos", strives to provide students with STEM education, problem solving, management, and leadership skills. We accomplish this by providing a student-led and student-focused team structure where people can find their place. Our family-like culture drives us to take pride in our team and give it our all.</p>
+			<p>FRC team 7243, the "Robo-Lobo-Bellos", strives to provide students with STEM education, problem solving, management, and leadership skills. We accomplish this by providing a student-led and student-focused team structure where people can find their place. Our family-like culture drives us to take pride in our team and give it our all.</p>
 
 		<h2>What We Do</h2>
 
--- src/pages/Partners.vue
+++ src/pages/Partners.vue
@@ -5,16 +5,16 @@
 					<h3>Alpha Wolf Partners: $2500+</h3>
 			<hr>
 			<div class='images'>
-				<img src='../assets/comcast.jpg' id="comcast">
-				<img src='../assets/NREL.png'>
+				<img src='../assets/comcast.jpg' id="comcast" alt="comcast">
+				<img src='../assets/NREL.png' alt="NREL">
 
 			</div>
 
 			<h3>Beta Wolf Partners: $2499-$1000</h3>
 			<hr>
 			<div class='images'>
-				<img src = "https://nec.dpsk12.org/wp-content/uploads/sites/147/NortheastEarlyCollegeLogo-01-scaled.jpg">
-				<img src='https://coloradosucceeds.org/wp-content/uploads/2019/01/WBL_Partners_DPSCareerConnect-300x200.jpg'>
+				<img src = "https://nec.dpsk12.org/wp-content/uploads/sites/147/NortheastEarlyCollegeLogo-01-scaled.jpg" alt="Northeast Early College">
+				<img src='https://coloradosucceeds.org/wp-content/uploads/2019/01/WBL_Partners_DPSCareerConnect-300x200.jpg' alt="DPS CareerConnect">
 			</div>
 
 			<h3>Delta Wolf Partners: $999-$500</h3>
