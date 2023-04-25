1e53862ef95a6dd3dfcceffa6940988302e5eead
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-04-25 17:13:55 -0600

    add robots route

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import First from "./pages/First.vue";
 import Home from  "./pages/Home.vue";
 import Partner from "./pages/Partner.vue";
 import Partners from  "./pages/Partners.vue";
+import Robots from  "./pages/Robots.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
@@ -15,6 +16,7 @@ const routes = [
   { path: "/first", component: First },
 	{ path: "/partners", component: Partners },
 	{ path: "/partner", component: Partner },
+  { path: "/robots", component: Robots },
 ];
 
 const router = createRouter({
