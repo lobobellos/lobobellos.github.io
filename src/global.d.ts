7fb1671e6c0e4e6a87663d982b3d19e9cf06a1bf
= <918780@dpsk12.net>
2023-06-08 17:14:50 -0600

    add vue type shims

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,8 @@
 declare module '*.jpg';
 declare module '*.png';
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
