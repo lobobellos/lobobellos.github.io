4f1b316b6b503c102adae4463ef66f9e832cb253
= <918780@dpsk12.net>
2023-06-08 20:01:16 -0600

    update header

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="header" >
-    <router-link to="/">
-      <img src="../assets/teamLogo.png" alt="team logo" >
-    </router-link>
+    <div class="imageContainer">
+      <router-link to="/">
+        <img src="../assets/teamLogo.png" alt="team logo" >
+      </router-link>
+    </div>
     <div class="title">
         <h1>FIRST Robotics Team 7243</h1>
         <p>Robo Lobo Bellos</p>
@@ -10,37 +12,35 @@
   </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style scoped>
+<style scoped lang="scss">
 
 p{
   font-size: medium;
 }
 .header{
-    display:flex;
-    
-    background: rgb(102,40,156);
-    background: linear-gradient(90deg, rgba(102,40,156,1) 0%, rgba(32,29,56,1) 100%);
+  display:flex;
+  background: rgb(102,40,156);
+  background: linear-gradient(90deg, rgba(102,40,156,1) 0%, rgba(32,29,56,1) 100%);
+  .imageContainer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+      img{
+      width:60%;
+      margin: auto;
+      display: block;
+    }
+  }
+  .title {
+      padding-left:5%;
+      text-align:left;
+      width:auto;
+      color:whitesmoke;
+      font-family: 'Open Sans', sans-serif;
+  }
 }
 
-.header img{
-	width:40%;
-	max-width: 150px;
-	max-height: 225px;
-	min-width:120px;
-	min-height:175px;
-}
-
-.header .title {
-    padding-left:5%;
-    text-align:left;
-    width:auto;
-    color:whitesmoke;
-    font-family: 'Open Sans', sans-serif;
-}
 
 h1,h3{
 	text-align:left;
