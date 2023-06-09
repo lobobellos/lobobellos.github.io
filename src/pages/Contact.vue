c627d2215f74293a32cd19bd844fbddf2ad6396a
= <918780@dpsk12.net>
2023-06-08 20:26:54 -0600

    scss rewrite

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,7 +2,7 @@
 	<h2>Contact Us</h2>
 	<h3>Multiple team representatives would love to get in contact with you</h3>
 	<div class="people">
-		<div v-for="p in people">
+		<div v-for="p in people" class="person">
 			<h3>{{ p.name }}</h3>
 			<h4>{{ p.title }}</h4>
 			<h4>{{ p.email }}</h4>
@@ -39,62 +39,25 @@ export default {
 }
 </script>
 
-<style>
-.body {
-	background-color: rgb(62, 16, 88);
-
-	display: flex;
-}
-
-h2 {
-	text-align: center;
-	color: white;
-	font-family: 'Figtree', sans-serif;
-}
-
-h3 {
+<style scoped lang="scss">
+h2,
+h3,
+h4 {
 	color: whitesmoke;
 	text-align: center;
 	font-family: 'Figtree', sans-serif;
 }
-
-.left {
-	flex: 25%;
-}
-
-.right {
-	flex: 25%;
-}
-
-table {
-	flex: 50%;
-	min-width: 600px;
-	font-family: 'Figtree', sans-serif;
-	border: 2px solid white;
-	color: white;
-}
-
-td {
-	border-bottom: 2px solid white;
-	padding-bottom: 10px;
-}
-
-.bottom td {
-	border-bottom: none;
-}
-
 .people {
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: center;
-}
-
-.people div {
-	text-align: center;
-	border: 4px outset rgb(100, 100, 100);
-	margin: 15px;
-	padding: 15px;
-	min-width: 285px;
-	color: white;
+	.person {
+		text-align: center;
+		border: 4px outset rgb(100, 100, 100);
+		margin: 15px;
+		padding: 15px;
+		min-width: 285px;
+		color: white;
+	}
 }
 </style>
