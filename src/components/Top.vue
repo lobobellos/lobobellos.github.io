051c1988bb2af27fa5eb86a76f71c4fc34c4deba
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-04 12:20:20 -0600

    remove imageContainer

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,15 +1,12 @@
 <template>
 	<div class="header">
-		<div class="imageContainer">
-			<router-link to="/">
-				<img src="../assets/teamLogo.png" alt="team logo" />
-			</router-link>
-		</div>
+		
 		<div class="title">
 			<h1>FIRST Robotics Team 7243</h1>
 			<p>Robo Lobo Bellos</p>
 		</div>
 	</div>
+	
 </template>
 
 <style scoped lang="scss">
@@ -17,24 +14,8 @@ p {
 	font-size: medium;
 }
 .header {
+	width: max-content;
 	display: flex;
-	background: rgb(102, 40, 156);
-	background: linear-gradient(
-		90deg,
-		rgba(102, 40, 156, 1) 0%,
-		rgba(32, 29, 56, 1) 100%
-	);
-	.imageContainer {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		img {
-			border-radius: 10%;
-			width: 60%;
-			margin: auto;
-			display: block;
-		}
-	}
 	.title {
 		padding-left: 5%;
 		text-align: left;
