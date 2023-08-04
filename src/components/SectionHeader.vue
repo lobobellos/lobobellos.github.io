15b141b4deaa48b32ed6e8d16d7c1f7194f557d7
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-04 15:57:32 -0600

    add subtitle

--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -5,12 +5,17 @@ const props = defineProps({
     type:String,
     required:true,
   },
+  subtitle:{
+    type:String,
+    required:false,
+  }
 })
 </script>
 
 <template>
   <h1>{{ props.title }}</h1>
 	<hr />
+  <h3>{{ props.subtitle }}</h3>
 </template>
 
 <style scoped lang="scss">
@@ -19,5 +24,10 @@ const props = defineProps({
   }
   hr {
     width: 80%;
+  }
+  h3{
+	text-align: center;
+	margin-right:10%;
+	margin-left:10%
   }
 </style>
