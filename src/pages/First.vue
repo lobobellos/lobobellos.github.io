988ac6116c9bc27a36cdc1d65fa63ef050fc0a57
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-04 20:04:35 -0600

    flexbox refactor

--- src/pages/First.vue
+++ src/pages/First.vue
@@ -3,28 +3,16 @@ import SectionHeader from '../components/SectionHeader.vue';
 </script>
 
 <template>
+	<div class="titleContainer">
+		<a href="https://www.firstinspires.org">
+			<img alt="FIRST" src="../assets/FIRST-logo.png" />
+		</a>
+		<div class="secHead">
+			<SectionHeader title="What Is FIRST?"/>
+		</div>
+	</div>
 	<div class="body">
 		<div class="textContainer">
-			<div class="titleContainer">
-				<a href="https://www.firstinspires.org">
-					<img alt="FIRST" src="../assets/FIRST-logo.png" />
-				</a>
-				<div class="secHead">
-					<SectionHeader title="What Is FIRST?"/>
-				</div>
-			</div>
-			<iframe
-				frameborder="0"
-				height="315"
-				src="https://www.youtube.com/embed/WlK7S7-Kx70"
-				style="
-					display: block;
-					margin-left: auto;
-					margin-right: auto;
-					max-width: 100%;
-				"
-				width="560"
-			></iframe>
 			<h2>Mission</h2>
 			<p>
 				The mission of&nbsp;
@@ -51,33 +39,20 @@ import SectionHeader from '../components/SectionHeader.vue';
 					&nbsp; Dean Kamen, Founder
 				</p>
 			</blockquote>
-			<h2>
-				Methodology
-			</h2>
-			<p>
-				<em>FIRST</em> Engages
-				<a href="https://www.firstinspires.org/about/at-a-glance">
-					PreK-12 (ages 4-18)&nbsp;students
-				</a>
-				in exciting, mentor-based, research and robotics programs that help them
-				become science and technology leaders, as well as well-rounded
-				contributors to society.
-			</p>
-			<p>
-				<em>FIRST</em>
-				works because of its use of strategies known to increase student
-				interest in STEM: hands-on learning, working as a team on real-life
-				problems, exposure to careers and adult mentors, emphasis on
-				<em>FIRST</em>
-				Core Values, and a culminating celebration where students can showcase
-				what they created and learned.&nbsp;
-				<br />
-				<br />
-				<em>FIRST</em>
-				participants also develop skills in teamwork, problem solving, and
-				communication, preparing them for success in school and the workforce,
-				no matter what path they take.
-			</p>
+		</div>
+		<div class="iframeContainer">
+			<iframe
+				frameborder="0"
+				height="315"
+				src="https://www.youtube.com/embed/WlK7S7-Kx70"
+				style="
+					display: block;
+					margin-left: auto;
+					margin-right: auto;
+					max-width: 100%;
+				"
+				width="560"
+			></iframe>
 		</div>
 	</div>
 </template>
@@ -86,19 +61,6 @@ import SectionHeader from '../components/SectionHeader.vue';
 a {
 	color: orange;
 	font-family: 'Figtree', sans-serif;
-}
-.body {
-	background-color: rgb(62, 16, 88);
-	text-align: center;
-	.textContainer {
-		width: 80%;
-		margin: auto;
-		max-width: 600px;
-	}
-	.textContainer {
-		color: white;
-		font-family: 'Figtree', sans-serif;
-	}
 }
 .titleContainer {
 	display: flex;
@@ -112,5 +74,26 @@ a {
 	h1 {
 		margin: 0;
 	}
+}
+.body {
+
+	display:flex;
+	flex-direction: row;
+	flex-wrap: wrap-reverse;
+	justify-content: space-between;
+	
+	.textContainer {
+		width: 80%;
+		margin: auto;
+		max-width: 600px;
+		color: white;
+		font-family: 'Figtree', sans-serif;
+	}
+	.iframeContainer {
+		width: 80%;
+		margin: auto;
+		max-width: 600px;
+	}
+	
 }
 </style>
