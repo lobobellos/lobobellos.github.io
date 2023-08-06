fab3ce49b1664e344c0b73a1ef5ea493da796c5b
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-05 20:18:30 -0600

    add donation link

--- src/components/DonateButton.vue
+++ src/components/DonateButton.vue
@@ -1,27 +1,25 @@
 <template>
- <a href="">
-  <button>
-    Donate
-  </button>
- </a>
+	<a href="https://www.gofundme.com/f/support-our-schools-robotics-team" target="_blank">
+		<button>Donate</button>
+	</a>
 </template>
 
 <style scoped lang="scss">
-a button{
-  background-color: orange;
-  background: linear-gradient(
-    45deg,
-    orange 0%,
-    rgb(151, 85, 42) 50%,
-    orange 100%
-  );
-  padding:1rem;
-  font-size: 1.2rem;
-  font-weight: 600;
-  border: none;
-  border-radius: 0.4rem;
-  &:hover{
-    box-shadow: 0px 0px 4px 5px #ffffff5e;
-  }
+a button {
+	background-color: orange;
+	background: linear-gradient(
+		45deg,
+		orange 0%,
+		rgb(151, 85, 42) 50%,
+		orange 100%
+	);
+	padding: 1rem;
+	font-size: 1.2rem;
+	font-weight: 600;
+	border: none;
+	border-radius: 0.4rem;
+	&:hover {
+		box-shadow: 0px 0px 4px 5px #ffffff5e;
+	}
 }
 </style>
