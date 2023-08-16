875c8c863e03fffa99d92710b68b5d4e891ce61b
= <918780@dpsk12.net>
2023-08-16 09:26:19 -0600

    add wolf head anim

--- components/Top.vue
+++ components/Top.vue
@@ -1,8 +1,13 @@
 <template>
 	<div class="header">
 		<div class="title">
-			<h1>FIRST Robotics Team <br>7243</h1>
-			<p>Robo Lobo Bellos</p>
+			<div class="text">
+				<h1>FIRST Robotics Team <br>7243</h1>
+				<p>Robo Lobo Bellos</p>
+			</div>
+		</div>
+		<div class="content">
+			<img src="~/assets/wolfhead.png" alt="wolf head logo">
 		</div>
 	</div>
 </template>
@@ -13,18 +18,49 @@ p {
 }
 .header {
 	display: flex;
+	flex-direction: row;
 	.title {
-		padding-left: 5%;
-		text-align: left;
-		color: whitesmoke;
-		font-family: 'Open Sans', sans-serif;
-		h1 {
-			margin-top: 0px;
-			word-wrap: break-word;
-			white-space: normal;
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		width: fit-content;
+		flex:content;
+		.text {
+			padding-left: 5%;
+			text-align: left;
+			color: whitesmoke;
+			font-family: 'Open Sans', sans-serif;
+			h1 {
+				margin-top: 0px;
+				word-wrap: break-word;
+				white-space: normal;
+			}
+			p {
+				margin-bottom: 0.5rem;
+			}
 		}
-		p {
-			margin-bottom: 0.5rem;
+	}
+	.content {
+		flex-grow: 1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		$dist : 20px;
+		img{
+			width: 100%;
+			max-width: 300px;
+			animation: float 5s ease-in-out 0s infinite alternate;
+		}
+		@keyframes float {
+			0% {
+				transform: translatey($dist);
+			}
+			100% {
+				transform: translatey(-$dist);
+			}
+		}
+		@media (max-width: 800px) {
+			display:none;
 		}
 	}
 }
