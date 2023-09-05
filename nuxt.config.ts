878d6a695087a7b7706f03306adec04f04c15b28
Replit user <>
2023-09-05 16:42:42 +0000

    add sitemap generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   //devtools: { enabled: true },
+	modules: ['nuxt-simple-sitemap'],
   vite:{
     build:{
       minify:true,
     },
+  },
+	site: {
+    url: 'https://lobobellos.github.io',
   },
   app: {
     head: {
